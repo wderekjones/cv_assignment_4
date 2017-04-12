3ea62edf4592786b4dc4af05f20b3b2a59ebccd5
Derek <derekjones2025@gmail.com>
2017-04-12 00:42:14 -0400

    making progress...

--- source/part2.py
+++ source/part2.py
@@ -1,27 +1,19 @@
-import numpy as np
-import matplotlib.pyplot as plt
-
-
 from sklearn import svm
-from skimage.io import imread,imshow
+from sklearn.metrics import accuracy_score
 from utils import *
-import h5py
 
 
 surf_train_data,labels1 = make_dataset("surf")
-alex_train_data, labels2 = make_dataset("alex")
 
 clf = svm.SVC()
-clf.fit(alex_train_data,labels1)
+clf.fit(surf_train_data,labels1)
 
 # evaluate classifier performance
 num_correct = 0
 
-for i in xrange(0,alex_train_data.shape[0]):
-    pred = clf.predict(alex_train_data[i,:])
-    if pred == labels1[i]:
-        num_correct +=1
 
-print "surf features accuracy: "+str(num_correct/float(surf_train_data.shape[0]))
+preds = clf.predict(surf_train_data)
 
+surf_accuracy = accuracy_score(labels1,preds)
 
+print "surf features accuracy: "+str(surf_accuracy)
--- source/utils.py
+++ source/utils.py
@@ -3,6 +3,9 @@ import pandas as pd
 import numpy as np
 
 
+from sklearn.cluster import KMeans
+
+
 def make_dataset(feature_type):
     train_data = pd.read_csv("data/metadata/train.csv")
 
@@ -15,25 +18,55 @@ def make_dataset(feature_type):
         feats_i = np.transpose(feats_i)
         if feats_i.shape[0] < min_feats:
             min_feats = feats_i.shape[0]
-        #print feats_i.shape[0]
         data_size+=1
 
     features = []
 
     if feature_type == "surf":
         i = 0
-        features = np.zeros([data_size, (64 * min_feats)])
+
+        codebook_feats = []
+        for filepath in train_data["filename"]:
+            feats_i = load_surf_features(filepath)
+            feats_i = np.transpose(feats_i)
+
+            feats_i = feats_i[0:min_feats,:]
+            for feat in feats_i:
+                codebook_feats.append(feat)
+
+            i+=1
+        codebook_feats = np.asarray(codebook_feats)
+        codebook = make_codebook(50,codebook_feats)
+
+        del codebook_feats
+
+        # for each of the training examples, using the codebook, make a histogram over the set of all of the respective surf features. normalize this and consider as the corresponding feature vector
+
+        features = np.zeros([train_data.shape[0],50])
+
+        i = 0
 
         for filepath in train_data["filename"]:
             feats_i = load_surf_features(filepath)
             feats_i = np.transpose(feats_i)
-            feats_i = feats_i[0:min_feats,:]
-            feats_i = np.ndarray.flatten(feats_i)
-            features[i] = feats_i
-            i+=1
+
+            hgram_i = np.zeros([1,50])
+
+            denom = feats_i.shape[0]
+
+            for feat in feats_i:
+                f_i = codebook.predict([feat])
+                hgram_i[0,f_i] +=1
+
+            hgram_i = np.divide(hgram_i,float(denom))
+
+            features[i] = hgram_i
+
+        features = np.asarray(features)
+        print  features.shape
     elif feature_type == "alex":
         i = 0
-        features = np.zeros([data_size,4096])
+        features = np.zeros([data_size,4096]) # try making this a smaller number in order to imporve speed, try dimensionality reduction?
 
         for filepath in train_data["filename"]:
             feats_i = load_alex_net_image_features(filepath)
@@ -41,6 +74,11 @@ def make_dataset(feature_type):
             features[i] = feats_i
             i+=1
     return features,labels
+
+def make_codebook(num_words,X):
+    cluster = KMeans(n_clusters=num_words)
+    cluster.fit(X)
+    return cluster
 
 def load_surf_features(path):
     path = path+".h5"
