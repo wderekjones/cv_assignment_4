9827fc84e0442a3c01a22c20154d55ab4fa034b3
Derek <derekjones2025@gmail.com>
2017-04-12 04:01:30 -0400

    making progress...

--- source/part2.py
+++ source/part2.py
@@ -1,19 +1,25 @@
-from sklearn import svm
+from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import accuracy_score
 from utils import *
 
+codebook = make_codebook("data/metadata/train.csv",50,20)
 
-surf_train_data,labels1 = make_dataset("surf")
+surf_train_data,train_labels = get_codebook_features_labels("data/metadata/train.csv",codebook)
+surf_test_data,test_labels = get_codebook_features_labels("data/metadata/test.csv",codebook)
 
-clf = svm.SVC()
-clf.fit(surf_train_data,labels1)
+
+
+rForest_clf = RandomForestClassifier(n_estimators = 100)
+rForest_clf.fit(surf_train_data,train_labels)
 
 # evaluate classifier performance
-num_correct = 0
 
+forest_train_preds = rForest_clf.predict(surf_train_data)
+forest_train_accuracy = accuracy_score(train_labels,forest_train_preds)
 
-preds = clf.predict(surf_train_data)
+forest_test_preds = rForest_clf.predict(surf_test_data)
+forest_test_accuracy = accuracy_score(test_labels,forest_test_preds)
 
-surf_accuracy = accuracy_score(labels1,preds)
+print "random forest surf features accuracy (train): "+str(forest_train_accuracy)
+print "random forest surf features accuracy (test): "+str(forest_test_accuracy)
 
-print "surf features accuracy: "+str(surf_accuracy)
--- source/utils.py
+++ source/utils.py
@@ -1,84 +1,73 @@
 import h5py
+import itertools
+
 import pandas as pd
 import numpy as np
-
-
+import matplotlib.pyplot as plt
 from sklearn.cluster import KMeans
 
 
-def make_dataset(feature_type):
-    train_data = pd.read_csv("data/metadata/train.csv")
-
+def get_codebook_features_labels(path,codebook):
+    train_data = pd.read_csv(path)
     labels = train_data["classid"]
 
-    min_feats = 99999999
-    data_size = 0
+    features = np.zeros([train_data.shape[0],50])
+
+    i = 0
     for filepath in train_data["filename"]:
         feats_i = load_surf_features(filepath)
         feats_i = np.transpose(feats_i)
-        if feats_i.shape[0] < min_feats:
-            min_feats = feats_i.shape[0]
-        data_size+=1
 
-    features = []
+        hgram_i = np.zeros([1,50])
 
-    if feature_type == "surf":
-        i = 0
+        for feat in feats_i:
+            f_i = codebook.predict([feat])
+            hgram_i[0,f_i] +=1
 
-        codebook_feats = []
-        for filepath in train_data["filename"]:
-            feats_i = load_surf_features(filepath)
-            feats_i = np.transpose(feats_i)
+        hgram_i = np.divide(hgram_i,float(feats_i.shape[0]))
 
-            feats_i = feats_i[0:min_feats,:]
-            for feat in feats_i:
-                codebook_feats.append(feat)
+        features[i] = hgram_i   #try features[i,:] = hgram_i
+        i += 1
 
-            i+=1
-        codebook_feats = np.asarray(codebook_feats)
-        codebook = make_codebook(50,codebook_feats)
+    features = np.asarray(features)
 
-        del codebook_feats
-
-        # for each of the training examples, using the codebook, make a histogram over the set of all of the respective surf features. normalize this and consider as the corresponding feature vector
-
-        features = np.zeros([train_data.shape[0],50])
-
-        i = 0
-
-        for filepath in train_data["filename"]:
-            feats_i = load_surf_features(filepath)
-            feats_i = np.transpose(feats_i)
-
-            hgram_i = np.zeros([1,50])
-
-            denom = feats_i.shape[0]
-
-            for feat in feats_i:
-                f_i = codebook.predict([feat])
-                hgram_i[0,f_i] +=1
-
-            hgram_i = np.divide(hgram_i,float(denom))
-
-            features[i] = hgram_i
-
-        features = np.asarray(features)
-        print  features.shape
-    elif feature_type == "alex":
-        i = 0
-        features = np.zeros([data_size,4096]) # try making this a smaller number in order to imporve speed, try dimensionality reduction?
-
-        for filepath in train_data["filename"]:
-            feats_i = load_alex_net_image_features(filepath)
-            feats_i = np.ndarray.flatten(feats_i)
-            features[i] = feats_i
-            i+=1
     return features,labels
 
-def make_codebook(num_words,X):
-    cluster = KMeans(n_clusters=num_words)
-    cluster.fit(X)
+def get_alex_feats_labels(path):
+    train_data = pd.read_csv(path)
+    labels = train_data["classid"]
+
+    i = 0
+    features = np.zeros([train_data.shape[0],4096]) # try making this a smaller number in order to imporve speed, try dimensionality reduction?
+
+    for filepath in train_data["filename"]:
+        feats_i = load_alex_net_image_features(filepath)
+        feats_i = np.ndarray.flatten(feats_i)
+        features[i] = feats_i
+        i+=1
+    return features,labels
+
+
+def make_codebook(path,size,num_words):
+    data = pd.read_csv(path)
+
+    codebook_feats = []
+    for filepath in data["filename"]:
+        feats_i = load_surf_features(filepath)
+        feats_i = np.transpose(feats_i)
+
+        #rand_indices = np.random.randint(0, feats_i.shape[0], size=num_words)   #make sure this doesn't generate duplicates
+        if feats_i.shape[0] >= num_words: # throw out samples with smaller number of features than the number of features we would like to extract
+            rand_indices = np.random.choice(feats_i.shape[0],num_words,replace=False)
+            for index in rand_indices:
+                codebook_feats.append(feats_i[index])
+
+    codebook_feats = np.asarray(codebook_feats)
+    cluster = KMeans(n_clusters=size,max_iter=1000)
+    cluster.fit(codebook_feats)
+
     return cluster
+
 
 def load_surf_features(path):
     path = path+".h5"
@@ -89,6 +78,7 @@ def load_surf_features(path):
 
     return image_features
 
+
 def load_alex_net_image_features(path):
     path = path +".h5"
     image_features = h5py.File(path)
@@ -97,3 +87,38 @@ def load_alex_net_image_features(path):
     image_features = image_features[0]
 
     return image_features
+
+
+def plot_confusion_matrix(cm, classes,
+                          normalize=False,
+                          title='Confusion matrix',
+                          cmap=plt.cm.Blues):
+    """
+    This function prints and plots the confusion matrix.
+    Normalization can be applied by setting `normalize=True`.
+    """
+    plt.imshow(cm, interpolation='nearest', cmap=cmap)
+    plt.title(title)
+    plt.colorbar()
+    tick_marks = np.arange(len(classes))
+    plt.xticks(tick_marks, classes, rotation=45)
+    plt.yticks(tick_marks, classes)
+
+    if normalize:
+        cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
+        print("Normalized confusion matrix")
+    else:
+        print('Confusion matrix, without normalization')
+
+    print(cm)
+
+    thresh = cm.max() / 2.
+    for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
+        plt.text(j, i, cm[i, j],
+                 horizontalalignment="center",
+                 color="white" if cm[i, j] > thresh else "black")
+
+    plt.tight_layout()
+    plt.ylabel('True label')
+    plt.xlabel('Predicted label')
+
